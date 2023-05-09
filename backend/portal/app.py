a364e41fb027791eb6df406939de89e9394682b2
achraf farhat <farhatachraf99@gmail.com>
2023-05-09 22:12:47 +0100

    updating the streamlit app reports (adding base station and cells parameters)

--- backend/portal/app.py
+++ backend/portal/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 import os
+from datetime import timedelta
+
 
 # Mapping of full region names to their abbreviations
 region_mapping = {
@@ -40,22 +42,39 @@ def load_csv_data(region_abbr, folder_path):
         return df
     return None
 
-def filter_data(df, date):
-    filtered_data = df[df["Time"].str.startswith(date)]
+def filter_data(df, start_date, end_date, base_station, cell):
+    filtered_data = df[(df["Time"].between(start_date, end_date)) & (df["eNodeB Name"] == base_station) & (df["Cell Name"] == cell)]
     return filtered_data
 
 # List all available regions
 regions = list(region_mapping.keys())
 selected_region = st.sidebar.selectbox("Choose a region", regions)
 selected_date = st.sidebar.date_input("Choose a date")
+time_period = st.sidebar.selectbox("Choose a time period", ["Daily", "Weekly", "Monthly"])
+
+if time_period == "Daily":
+    start_date = selected_date.strftime('%m-%d-%Y')
+    end_date = selected_date.strftime('%m-%d-%Y')
+elif time_period == "Weekly":
+    start_date = selected_date.strftime('%m-%d-%Y')
+    end_date = (selected_date + timedelta(days=6)).strftime('%m-%d-%Y')
+else:  # Monthly
+    start_date = selected_date.strftime('%m-%d-%Y')
+    end_date = (selected_date + timedelta(days=30)).strftime('%m-%d-%Y')
 
 # Load CSV file for the selected region
 region_abbr = region_mapping[selected_region]
 df = load_csv_data(region_abbr, 'csv_data')
 
 if df is not None:
-    # Filter data based on user-selected date
-    filtered_data = filter_data(df, selected_date.strftime('%m-%d-%Y'))
+    base_stations = df["eNodeB Name"].unique()
+    selected_base_station = st.sidebar.selectbox("Choose a base station", base_stations)
+    
+    cells = df[df["eNodeB Name"] == selected_base_station]["Cell Name"].unique()
+    selected_cell = st.sidebar.selectbox("Choose a cell", cells)
+    
+    # Filter data based on user-selected date range, base station, and cell
+    filtered_data = filter_data(df, start_date, end_date, selected_base_station, selected_cell)
 
     if not filtered_data.empty:
         # Create a line chart for FT_AVERAGE_NB_OF_USERS
@@ -68,10 +87,10 @@ if df is not None:
         # Create a line chart for FT_4G/LTE DL TRAFFIC VOLUME (GBYTES)
         traffic_fig = go.Figure()
         traffic_fig.add_trace(go.Scatter(x=filtered_data["Time"], y=filtered_data["FT_4G/LTE DL TRAFFIC VOLUME (GBYTES)"], mode='lines', name="4G LTE Downlink Traffic Volume"))
-        traffic_fig.update_layout(title="4G LTE Downlink Traffic Volume")
+        traffic_fig.update_layout(title="4G LTE Downlink Traffic Volume (GBYTES)")
 
         st.plotly_chart(traffic_fig)
     else:
-        st.write("No data available for the selected region and date.")
+        st.write("No data available for the selected region, date, base station, and cell.")
 else:
     st.write("No data available for the selected region.")
